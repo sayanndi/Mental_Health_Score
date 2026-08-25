9bbe484187c9faa96804a1ade8e1ea97a8e62922
sayan nandi <sayan.2022.nandi@gmail.com>
2026-08-25 12:18:55 +0530

    changed backend api link in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-  const API_BASE = "http://127.0.0.1:8000";
+  const API_BASE = "https://mental-health-score-tt9e.onrender.com";
   const PREDICT_URL = `${API_BASE}/predict`;
 
   const form = document.getElementById("predictForm");
